dba0491839c5fd0bee5ba10b3885e1ac05609fbd
Sergey <sergey.bv26@gmail.com>
2023-03-31 21:42:18 +0300

    add rocket

--- game.py
+++ game.py
@@ -1,14 +1,50 @@
 import asyncio
+from functools import partial
+from itertools import cycle
 import random
 import time
 import curses
+
+from environs import Env
 
 
 TIC_TIMEOUT = 0.1
 STAR_SYMBOLS = ['*', '+', '.', ':']
 
+def draw_frame(canvas, start_row, start_column, text, negative=False):
+    """Draw multiline text fragment on canvas, erase text instead of drawing if negative=True is specified."""
+
+    rows_number, columns_number = canvas.getmaxyx()
+
+    for row, line in enumerate(text.splitlines(), round(start_row)):
+        if row < 0:
+            continue
+
+        if row >= rows_number:
+            print('ERROR')
+            break
+
+        for column, symbol in enumerate(line, round(start_column)):
+            if column < 0:
+                continue
+
+            if column >= columns_number:
+                break
+
+            if symbol == ' ':
+                continue
+
+            # Check that current position it is not in a lower right corner of the window
+            # Curses will raise exception in that case. Don`t ask why…
+            # https://docs.python.org/3/library/curses.html#curses.window.addch
+            if row == rows_number - 1 and column == columns_number - 1:
+                continue
+
+            symbol = symbol if not negative else ' '
+            canvas.addch(row, column, symbol)
+
 async def sleep(seconds):
-    for _ in range(int(seconds * 1000)):
+    for _ in range(int(seconds * 10)):
         await asyncio.sleep(0)
 
 async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
@@ -17,10 +53,10 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
     row, column = start_row, start_column
 
     canvas.addstr(round(row), round(column), '*')
-    await sleep(1)
+    await asyncio.sleep(0)
 
     canvas.addstr(round(row), round(column), 'O')
-    await sleep(1)
+    await asyncio.sleep(0)
     canvas.addstr(round(row), round(column), ' ')
 
     row += rows_speed
@@ -35,10 +71,16 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
     while 0 < row < max_row and 0 < column < max_column:
         canvas.addstr(round(row), round(column), symbol)
-        await sleep(1)
+        await asyncio.sleep(0)
         canvas.addstr(round(row), round(column), ' ')
         row += rows_speed
         column += columns_speed
+
+async def animate_spaceship(canvas, row, column, frames):
+    for frame in cycle(frames):
+        draw_frame(canvas, row, column, frame)
+        await asyncio.sleep(0)
+        draw_frame(canvas, row, column, frame, negative=True)
 
 async def blink(canvas, row, column, symbol='*', state=0):
     while True:
@@ -63,38 +105,24 @@ async def blink(canvas, row, column, symbol='*', state=0):
                 state = 0
                 await sleep(0.3)
 
-def draw(canvas):
-    # row, column = (5, 20)
+def draw(canvas, frames):
+    coroutines = []
     canvas.border()
     curses.curs_set(False)
-    # while True:
-    #     canvas.addstr(row, column, '*', curses.A_DIM)
-    #     canvas.refresh()
-    #     time.sleep(2)
-    #     canvas.addstr(row, column, '*')
-    #     canvas.refresh()
-    #     time.sleep(0.3)
-    #     canvas.addstr(row, column, '*', curses.A_BOLD)
-    #     canvas.refresh()
-    #     time.sleep(0.5)
-    #     canvas.addstr(row, column, '*')
-    #     canvas.refresh()
-    #     time.sleep(0.3)
     star_quantity = random.randint(1, 100)
     rows, columns = canvas.getmaxyx()
     row_max, column_max = rows - 2, columns - 2
     row_center, column_center = row_max // 2, column_max // 2
     fire_coroutine = fire(canvas, row_center, column_center)
-    while True:
-        try:
-            fire_coroutine.send(None)
-            canvas.refresh()
-        except StopIteration:
-            break
-    coroutines = [blink(canvas, random.randint(2, row_max), random.randint(2, column_max),
-                        random.choice(STAR_SYMBOLS), random.randint(0, 3)) for _ in range(star_quantity)]
+    spaceship_coroutine = animate_spaceship(canvas, row_center, column_center, frames)
+
+    coroutines.append(fire_coroutine)
+    coroutines.append(spaceship_coroutine)
+    coroutines.extend([blink(canvas, random.randint(2, row_max), random.randint(2, column_max),
+                       random.choice(STAR_SYMBOLS), random.randint(0, 3)) for _ in range(star_quantity)])
 
     while True:
+        canvas.border()
         for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
@@ -103,8 +131,22 @@ def draw(canvas):
                 coroutines.remove(coroutine)
         if len(coroutines) == 0:
             break
-    time.sleep(TIC_TIMEOUT)
+        time.sleep(TIC_TIMEOUT)
+
+def main():
+    env = Env()
+    env.read_env()
+    frames_path = env('FRAMES_PATH')
+    frames = []
+    with open(f'{frames_path}rocket_frame_1.txt', 'r') as f:
+        frame = f.read()
+        frames.append(frame)
+    with open(f'{frames_path}rocket_frame_2.txt', 'r') as f:
+        frame = f.read()
+        frames.append(frame)
+    draw_partial = partial(draw, frames=frames)
+    curses.update_lines_cols()
+    curses.wrapper(draw_partial)
 
 if __name__ == '__main__':
-    curses.update_lines_cols()
-    curses.wrapper(draw)
+    main()
