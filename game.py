9f8f813742d07b75a3833fa44948d8f24f27b527
Sergey <sergey.bv26@gmail.com>
2023-04-09 11:00:58 +0300

    fix after codereview

--- game.py
+++ game.py
@@ -15,9 +15,13 @@ LEFT_KEY_CODE = 260
 RIGHT_KEY_CODE = 261
 UP_KEY_CODE = 259
 DOWN_KEY_CODE = 258
-STATES = {
-    "spaceship_state": ""
+STAR_DRAW_STATES = {
+    0: (curses.A_DIM, 2),
+    1: (curses.A_NORMAL, 0.3),
+    2: (curses.A_BOLD, 0.5),
+    3: (curses.A_NORMAL, 0.3)
 }
+BORDER_SIZE = 2
 
 
 def get_frame_size(text):
@@ -116,94 +120,68 @@ async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0
 
     curses.beep()
 
-    while 0 < row < max_row and 0 < column < max_column:
+    while BORDER_SIZE < row < max_row and BORDER_SIZE < column < max_column:
         canvas.addstr(round(row), round(column), symbol)
         await asyncio.sleep(0)
         canvas.addstr(round(row), round(column), ' ')
         row += rows_speed
         column += columns_speed
 
-async def animate_spaceship(frames):
-    for frame in cycle(frames):
-        STATES['spaceship_state'] = frame
-        await asyncio.sleep(0)
-
-async def move_spaceship(canvas):
+async def move_spaceship(canvas, frames):
     rows, columns = canvas.getmaxyx()
-    row_max, column_max = rows - 1, columns - 1
+    row_max, column_max = rows - BORDER_SIZE, columns - BORDER_SIZE
     row, column = row_max // 2, column_max // 2
+    frame_generator = cycle(frames)
 
     while True:
         rows_direction, columns_direction, space_pressed = read_controls(canvas)
-        frame = STATES['spaceship_state']
-        frame_rows, frame_columns = get_frame_size(frame)
+        current_frame = next(frame_generator)
+
+        frame_rows, frame_columns = get_frame_size(current_frame)
         row, column = row + rows_direction, column + columns_direction
 
-        if row + frame_rows // 2 >= row_max - 5:
-            row = row_max - frame_rows // 2 - 5
-        elif column + frame_columns // 2 >= column_max - 2:
-            column = column_max - frame_columns // 2 - 3
-        elif row - frame_rows // 2 <= - 3:
-            row = frame_rows // 2 - 3
-        elif column - frame_columns // 2 <= 0:
-            column = frame_columns // 2 + 1
-        draw_frame(canvas, row, column, frame)
-        previos_frame = frame
+        row = max(1, min(row, row_max - frame_rows))
+        column = max(1, min(column, column_max - frame_columns))
+
+        draw_frame(canvas, row, column, current_frame)
         await asyncio.sleep(0)
-        draw_frame(canvas, row, column, previos_frame, negative=True)
+        draw_frame(canvas, row, column, current_frame, negative=True)
 
 async def blink(canvas, row, column, symbol='*', state=0):
     while True:
-        match state:
-            case 0:
-                canvas.addstr(row, column, symbol, curses.A_DIM)
-                state = 1
-                await sleep(2)
-
-            case 1:
-                canvas.addstr(row, column, symbol)
-                state = 2
-                await sleep(0.3)
-
-            case 2:
-                canvas.addstr(row, column, symbol, curses.A_BOLD)
-                state = 3
-                await sleep(0.5)
-
-            case 3:
-                canvas.addstr(row, column, symbol)
-                state = 0
-                await sleep(0.3)
+        effect, delay = STAR_DRAW_STATES[state]
+        canvas.addstr(row, column, symbol, effect)
+        state = state + 1 if state < 3 else 0
+        await sleep(delay)
 
 def draw(canvas, frames):
     coroutines = []
     canvas.border()
+    canvas.nodelay(True)
     curses.curs_set(False)
     star_quantity = random.randint(1, 100)
     rows, columns = canvas.getmaxyx()
-    row_max, column_max = rows - 2, columns - 2
+    row_max, column_max = rows - BORDER_SIZE, columns - BORDER_SIZE
     row_center, column_center = row_max // 2, column_max // 2
     fire_coroutine = fire(canvas, row_center, column_center)
-    spaceship_coroutine = animate_spaceship(frames)
-    move_spaceship_coroutine = move_spaceship(canvas)
+    move_spaceship_coroutine = move_spaceship(canvas, frames)
 
     coroutines.append(fire_coroutine)
-    coroutines.append(spaceship_coroutine)
     coroutines.append(move_spaceship_coroutine)
-    coroutines.extend([blink(canvas, random.randint(2, row_max), random.randint(2, column_max),
-                       random.choice(STAR_SYMBOLS), random.randint(0, 3)) for _ in range(star_quantity)])
+    coroutines.extend([blink(canvas, random.randint(2, row_max),
+                             random.randint(2, column_max),
+                             random.choice(STAR_SYMBOLS),
+                             random.choice(list(STAR_DRAW_STATES.keys()))) for _ in range(star_quantity)])
 
     while True:
-        canvas.border()
-        canvas.nodelay(True)
         for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
-                canvas.refresh()
             except StopIteration:
                 coroutines.remove(coroutine)
         if len(coroutines) == 0:
             break
+        canvas.refresh()
         time.sleep(TIC_TIMEOUT)
 
 def main():
@@ -214,8 +192,10 @@ def main():
     with open(f'{frames_path}rocket_frame_1.txt', 'r') as f:
         frame = f.read()
         frames.append(frame)
+        frames.append(frame)
     with open(f'{frames_path}rocket_frame_2.txt', 'r') as f:
         frame = f.read()
+        frames.append(frame)
         frames.append(frame)
     draw_partial = partial(draw, frames=frames)
     curses.update_lines_cols()
